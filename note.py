fddae2cb6cd5f736928fcf4c79319783e9aa7c03
syeu <syeu3251@gmail.com>
2026-09-23 14:29:37 -0500

    made into a daily note with time stamps

--- note.py
+++ note.py
@@ -1,14 +1,11 @@
 from pathlib import Path
 import datetime
 import readline
-import os
-
-token = os.environ["GITHUB_TOKEN"]
 
 
-title = input("Note title: ").strip()
-body = input("Notes: ")
 now = datetime.datetime.now()
+title = now.strftime('%Y-%m-%d')
+body = input("Notes: ")
 duplicate = 0
 
 # Keep practice notes in a folder beside this script.
@@ -23,22 +20,28 @@ safe_title = "".join(
 
 note_path = notes_folder / f"{safe_title or 'Untitled'}.md"
 
-try:
-    # "x" creates a new file and prevents overwriting an existing note.
-    with note_path.open("x", encoding="utf-8") as note:
-        note.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')}\n\n{body}\n")
-    print(f"Created: {note_path}")
-except FileExistsError:
-    # If a note with the same title already exists, append a number to the filename.
-    while True:
-        duplicate += 1
-        note_path = notes_folder / f"{safe_title or 'Untitled'}_{duplicate}.md"
-        
-        try:
-            with note_path.open("x", encoding="utf-8") as note:
-                note.write(f"{now.strftime('%Y-%m-%d %H:%M:%S')}\n\n{body}\n")
-            print(f"Created: {note_path}")
 
-            break
-        except FileExistsError:
-            continue
+with note_path.open("a", encoding="utf-8") as note:
+    note.write(f"**{now.strftime('%H:%M:%S')}**\n{body}\n\n")
+print(f"Created: {note_path}")
+
+
+# try:
+#     # "x" creates a new file and prevents overwriting an existing note.
+#     with note_path.open("a", encoding="utf-8") as note:
+#         note.write(f"{now.strftime('%H:%M:%S')}\n\n{body}\n")
+#     print(f"Created: {note_path}")
+# except FileExistsError:
+#     # If a note with the same title already exists, append a number to the filename.
+#     while True:
+#         duplicate += 1
+#         note_path = notes_folder / f"{safe_title or 'Untitled'}_{duplicate}.md"
+        
+#         try:
+#             with note_path.open("x", encoding="utf-8") as note:
+#                 note.write(f"{now.strftime('%H:%M:%S')}\n\n{body}\n")
+#             print(f"Created: {note_path}")
+
+#             break
+#         except FileExistsError:
+#             continue
